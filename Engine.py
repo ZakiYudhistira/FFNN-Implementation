8d17dd34ed2caf9008eeec057f3bd7babe8fafb5
zaki_yudhistira <zakiyudhistira.itb69@gmail.com>
2025-03-17 22:33:18 +0700

    feat: weight initialization

--- Engine.py
+++ Engine.py
@@ -2,21 +2,55 @@ import numpy as np
 import random as rd
 
 class Layer:
-    def __init__(self, neurons, prev_neurons, activation_function):
+    def __init__(self, neurons, prev_neurons, activation_function, bias, init_type="random-uniform"):
         self.n_neurons = neurons
         self.prev_n_nodes = prev_neurons+1
         self.activation_function = activation_function
 
-        np.random.seed(20)
-        self.weight_matrix = np.random.rand(self.prev_n_nodes, neurons)
+        if init_type=="random-uniform":
+            self.initiateWeightRDUniform()
+        elif init_type=="random-normal":
+            self.initiateWeightRDNormal()
+        elif init_type=="xavier":
+            self.initiateWeightXavier()
+        elif init_type=="he":
+            self.initateWeightHe()
+        elif init_type=="zero":
+            self.initiateWeightZero()
+        else:
+            raise ValueError("Invalid intiation type")
+        
+        self.weight_matrix[-1, :] = bias
 
     def multiply(self, input_array):
         ret = np.matmul(input_array, self.weight_matrix)
         ret = self.activation_function(ret)
         return ret
+    
+    def initiateWeightRDUniform(self, seed=0, lower_bound=0, upper_bound=1):
+        np.random.seed(seed)
+        self.weight_matrix = np.random.uniform(lower_bound, upper_bound, (self.prev_n_nodes, self.n_neurons))
+    
+    def initiateWeightRDNormal(self, mean, variance, seed):
+        np.random.seed(seed)
+        self.weight_matrix = np.random.normal(mean, variance, (self.prev_n_nodes, self.n_neurons))
+    
+    def initiateWeightZero(self):
+        self.weight_matrix = np.zeros((self.prev_n_nodes, self.n_neurons))
+
+    def initiateWeightXavier(self, seed=0):
+        np.random.seed(seed)
+        fan_avg = (self.prev_n_nodes-1 + self.n_neurons)/2
+        variance = 1/fan_avg
+        self.weight_matrix = np.random.normal(0, variance, (self.prev_n_nodes, self.n_neurons))
+    
+    def initateWeightHe(self, seed=0):
+        np.random.seed(seed)
+        variance = 2/(self.prev_n_nodes-1)
+        self.weight_matrix = np.random.normal(0, variance, (self.prev_n_nodes, self.n_neurons))
 
 class NeuralNetwork:
-    def __init__(self, n_input, n_output, n_hiddenlayer, hidden_layers_size, hidden_layers_function, output_layer_function):
+    def __init__(self, n_input, n_output, n_hiddenlayer, hidden_layers_size, hidden_layers_function, output_layer_function, bias, init_type):
         self.n_input = n_input
         self.n_output = n_output
         self.n_hiddenlayer = n_hiddenlayer
@@ -24,14 +58,14 @@ class NeuralNetwork:
         self.hidden_layers_function = hidden_layers_function
         self.output_layer_function = output_layer_function
         
-        self.initiateLayers()
+        self.initiateLayers(bias)
         
-    def initiateLayers(self):
+    def initiateLayers(self, bias):
         self.layers = []
-        self.layers.append(Layer(self.hidden_layers_size[0], self.n_input, self.hidden_layers_function[0]))
+        self.layers.append(Layer(self.hidden_layers_size[0], self.n_input, self.hidden_layers_function[0], bias, init_type))
         for i in range(1, self.n_hiddenlayer):
-            self.layers.append(Layer(self.hidden_layers_size[i], self.hidden_layers_size[i-1], self.hidden_layers_function[i]))
-        self.layers.append(Layer(self.n_output, self.hidden_layers_size[-1], self.output_layer_function))
+            self.layers.append(Layer(self.hidden_layers_size[i], self.hidden_layers_size[i-1], self.hidden_layers_function[i], bias, init_type))
+        self.layers.append(Layer(self.n_output, self.hidden_layers_size[-1], self.output_layer_function, bias, init_type))
     
     def forward(self, input_array):
         for layer in self.layers:
@@ -39,9 +73,23 @@ class NeuralNetwork:
             input_array = layer.multiply(input_array)
         return input_array
 
-input = np.array([1,2,3,4,5,6])
+array_input = np.array([2,2,3,4,5,-1])
+outputs = 7
+hidden_layers = 5
+hidden_sizes = [4, 3, 1, 6, 4]
+bias=5
+init_type="he"
 
 function = lambda x:x
+hidden_function = [function, function, function, function, function]
 
-neural = NeuralNetwork(n_input=len(input), n_output=7, n_hiddenlayer=5, hidden_layers_size=[4, 3, 1, 6, 4], hidden_layers_function=[function, function, function, function, function], output_layer_function=function)
-print(neural.forward(input))
+neural = NeuralNetwork(n_input=len(array_input),
+                       n_output=outputs,
+                       n_hiddenlayer=hidden_layers,
+                       hidden_layers_size=hidden_sizes,
+                       hidden_layers_function=hidden_function,
+                       output_layer_function=function,
+                       bias=bias,
+                       init_type=init_type)
+
+print(neural.forward(array_input))
